d0e8ef4874e63c40f9a5db22726e931f78d9a8e0
gmateu <gmateu@esliceu.com>
2023-03-19 08:56:04 +0100

    creades constants

--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
 #crear virtual env: python3 -m venv venv
 #utilitzar virtual env: source venv/bin/activate
 #imports
+from constants import *
 from dydx3 import Client
 from web3 import Web3
 from pprint import pprint
@@ -8,24 +9,8 @@ from datetime import datetime,timedelta
 from dydx3.constants import MARKET_BTC_USD
 
 
-#use testnet
-from dydx3.constants import API_HOST_GOERLI
 
-#use mnainet
-from dydx3.constants import API_HOST_MAINNET
 
-#CONSTANTS  
-ETHEREUM_ADDRESS = "0x6680beE0202cb15eD12Bf27A7919A30128628814"
-ETH_PRIVATE_KEY = "0X7a130dd03df81cac3918f81e392e38132536e8814d7f59b83d13941d3637c183"
-
-STARK_PRIVATE_KEY = "03c57e02bd988bcf236f6662fa1f3c5c21167f86fc50ff1a787a002861d19c93"
-DYDX_API_KEY = "2ac602b5-c7b9-c566-fa31-cddf664e3251"
-DYDX_API_SECRET = "RQrd7zqwJxltcrZg5Ly0D1YVMjcNMjzIX3tGjY9c"
-DYDX_API_PASSPHRASE = "Zmkhz8OxXUbAC1Uk4l3i"
-HOST = API_HOST_GOERLI
-
-#provider
-HTTP_PROVIDER = "https://eth-goerli.g.alchemy.com/v2/cukxgabNFOTQDa8gjlREHzeaSKDjK-om"
 
 #Create client connection
 client = Client(
