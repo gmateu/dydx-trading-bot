67637c3663d261525e697e1075d73075b672595c
gmateu <gmateu@esliceu.com>
2023-03-20 06:28:31 +0100

    ja se guardes els csv

--- program/main2.py
+++ program/main2.py
@@ -1,6 +1,7 @@
 from func_connections import *
 from func_private import *
 from func_public import *
+from func_cointegration import *
 
 
 if __name__ == '__main__':
@@ -33,4 +34,28 @@ if __name__ == '__main__':
             # close_prices = get_candles_historical(client,"SOL-USD")
         except Exception as e:
             print("error fetching prices",e)
+            exit(1)
+
+        # Store Cointegrated Pairs
+        try:
+            print("Storing cointegrated pairs...")
+            stores_result = store_cointegration_results(df_market_prices)
+            if stores_result != "saved":
+                print("Error saving cointegrated pairs")
+                exit(1)
+        except Exception as e:
+            print("Error saving cointegrated pairs: ", e)
+            #send_message(f"Error saving cointegrated pairs {e}")
+            exit(1)
+
+
+
+        
+
+    #place trades for opening positions
+    if PLACE_TRADES:
+        try:
+
+        except Exception as e:
+            print("error trading ", e)
             exit(1)
