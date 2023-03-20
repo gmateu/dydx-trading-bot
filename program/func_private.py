e2498da539b219c59482f7a87e397fb2637440ed
gmateu <gmateu@esliceu.com>
2023-03-20 05:28:50 +0100

    ja fa el csv

--- program/func_private.py
+++ program/func_private.py
@@ -3,6 +3,13 @@ import time
 from pprint import pprint
 from func_utils import *
 
+
+#check order status
+def check_order_status(client,order_id):
+    order = client.private.get_order_by_id(order_id)
+    return order.data["order"]["status"]
+
+
 #place market order
 def place_market_order(client,market,side,size,price,reduce_only):
     account = client.private.get_account()
