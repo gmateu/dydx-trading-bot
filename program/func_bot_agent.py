85f33e3d52c9b6aa9bfcc7a8cd7d4e624a09a349
gmateu <gmateu@esliceu.com>
2023-03-20 05:34:06 +0100

    llevam coses

--- program/func_bot_agent.py
+++ program/func_bot_agent.py
@@ -2,8 +2,68 @@
 
 from func_private import place_market_order, check_order_status
 from datetime import datetime, timedelta
-from func_messaging import send_message
 import time
 
 from pprint import pprint
 
+# Class: Agent for managing opening and checking trades
+class BotAgent:
+
+  """
+    Primary function of BotAgent handles opening and checking order status
+  """
+
+  # Initialize class
+  def __init__(
+    self,
+    client,
+    market_1,
+    market_2,
+    base_side,
+    base_size,
+    base_price,
+    quote_side,
+    quote_size,
+    quote_price,
+    accept_failsafe_base_price,
+    z_score,
+    half_life,
+    hedge_ratio,
+  ):
+    # Initialize class variables
+    self.client = client
+    self.market_1 = market_1
+    self.market_2 = market_2
+    self.base_side = base_side
+    self.base_size = base_size
+    self.base_price = base_price
+    self.quote_side = quote_side
+    self.quote_size = quote_size
+    self.quote_price = quote_price
+    self.accept_failsafe_base_price = accept_failsafe_base_price
+    self.z_score = z_score
+    self.half_life = half_life
+    self.hedge_ratio = hedge_ratio
+
+    # Initialze output variable
+    # Pair status options are FAILED, LIVE, CLOSE, ERROR
+    self.order_dict = {
+      "market_1": market_1,
+      "market_2": market_2,
+      "hedge_ratio": hedge_ratio,
+      "z_score": z_score,
+      "half_life": half_life,
+      "order_id_m1": "",
+      "order_m1_size": base_size,
+      "order_m1_side": base_side,
+      "order_time_m1": "",
+      "order_id_m2": "",
+      "order_m2_size": quote_size,
+      "order_m2_side": quote_side,
+      "order_time_m2": "",
+      "pair_status": "",
+      "comments": "",
+    }
+
+if __name__ == "__main__":
+    print("test")
